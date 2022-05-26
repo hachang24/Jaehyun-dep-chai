b9d06d90540e0be11c0613b69d10e0c6f02ba842
hachang24 <hthhuyntrang@gmail.com>
2022-05-26 19:32:56 +0700

    add f2

---
+++ demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int f2(int x, int y){
+    int sum = 0;
+    return sum = x + y;
+}
+
+int main()
+{
+    int x, y;
+    printf("Hello F-Code:\n");
+    nameGroup();
+    f2(x, y);
+    // f3();
+    // f4();
+    return 0;
+}
